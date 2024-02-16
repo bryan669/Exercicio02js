6bc3d8811c930fa145be5bbeecd7fb29fe9dccc9
bryan669 <144687388+bryan669@users.noreply.github.com>
2024-02-16 20:37:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var total = 10 + 5 * 2 / 2 + 20;
+console.log(total)
+var idade = 'dezoito 18'
+console.log(isNaN(idade))
+var valor = '100blr'
+console.log(isNaN(valor))
+var somar = + '200' + 50;
+console.log(somar)
+var incremento = 5;
+console.log(++incremento)
+var numero = '80';
+var unidade = 'kg';
+var peso = numero / 2;
+var pesoDivididoPorDois = peso + unidade;
+console.log(pesoDivididoPorDois)
